7649aa8f4112fcbcbd4278a2bb705eb732e43ab4
Olivier Nguyen <nguyenolive@gmail.com>
2015-09-22 01:23:25 -0400

    up to crossvalidation

---
+++ assignment1/KFoldCV.m~
@@ -0,0 +1,6 @@
+function [ output_args ] = KFoldCV( x, y, k )
+%KFOLDCV Performs k-fold cross-validation
+for n = 1:k:length(x)
+
+end
+
--- assignment1/PolyRegress.m
+++ assignment1/PolyRegress.m
@@ -1,7 +1,6 @@
 function [ w ] = PolyRegress( x, y, d )
 %POLYREGRESS 
-X = ones(length(x), 1);
-for n = 1:d
+for n = 0:d
     X = horzcat(x.^n, X);
 end
 w = inv((transpose(X) * X)) * X.' * y;
