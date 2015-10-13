1e14bd79bfc170ac0bd831e18aac9b129ba7f85d
Olivier Nguyen <nguyenolive@gmail.com>
2015-10-13 19:04:54 -0400

    plotting cost over iterations for q5

--- assignment2/question5.m
+++ assignment2/question5.m
@@ -8,17 +8,25 @@ w = normrnd(0, 1, [num_features 1]) * 0.001; % generate random starting W from n
 
 % Find best learning rate
 lr = [1e-9 1e-7 1e-5 1e-3 1e-1];
-num_iterations = 50000;
+num_iterations = 1000;
 best_lr = 10;
 best_loss = +Inf;
+
 for i = 1:length(lr);
-   for j = 1:num_iterations
+    for j = 1:num_iterations
        [J, grad] = LRCostFunction(w, x, y);
 %       disp(J);
-       w = w + lr(i) * grad;
+        cost(j, i) = J;
+        w = w + lr(i) * grad;
    end
    if (J < best_loss)
       best_loss = J;
       best_lr = lr(i);
+      best_lr_index = i;
    end
 end
+
+plot(cost(:, best_lr_index));
+title('Training error over iterations for lr = 1e-3');
+xlabel('Iterations');
+ylabel('J');
