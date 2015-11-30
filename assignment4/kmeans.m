c9591e51e2e22c0da312a36e24bea5553c47c913
Olivier Nguyen <nguyenolive@gmail.com>
2015-11-30 03:01:09 -0500

    done q3

--- assignment4/kmeans.m
+++ assignment4/kmeans.m
@@ -1,12 +1,13 @@
 function [ cluster_idx, centroids ] = kmeans(x, k, centroids)
     [num_samples, num_feat] = size(x);
     cluster_idx = zeros(num_samples, 1);
-    epochs = 1;
+    iterations = 100;
+    epsilon = 0.01
     initial_centroids = centroids;
     dist = zeros(k, 1);
    
-    for epoch = 1:epochs
-        disp(epoch);
+    for it = 1:iterations
+        disp(it);
         % Assign each sample to the nearest cluster
         for i = 1:num_samples
             for j = 1:k
@@ -19,13 +20,18 @@ function [ cluster_idx, centroids ] = kmeans(x, k, centroids)
         for i=1:k
             indices = find(cluster_idx == i);
             if isempty(indices)
-               centroids(i, :) = initial_centroids(i, :);
+               new_centroids(i, :) = initial_centroids(i, :);
             else
-               centroids(i, :) = mean(x(indices, :));
-               ssd = norm(bsxfun(@minus, centroids(i, :), x(indices, :)))^2;
+               new_centroids(i, :) = mean(x(indices, :));
+               ssd = norm(bsxfun(@minus, new_centroids(i, :), x(indices, :)))^2;
                disp(ssd);
             end
         end
+        disp(norm(new_centroids - centroids));
+        if norm(new_centroids - centroids) <= epsilon
+           break; 
+        end
+        centroids = new_centroids;
     end
 end 
 
